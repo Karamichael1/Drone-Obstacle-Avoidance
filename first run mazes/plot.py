8552b28f108513c8bcefdb73ffecd53f81c8fc0b
nick-karamichael <karamichaeln@gmail.com>
2024-10-21 11:18:40 +0200

    try

--- first run mazes/plot.py
+++ first run mazes/plot.py
@@ -2,16 +2,16 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-astar_df = pd.read_csv('run3/astardwa_maze_simulation_results.csv')
-custom_df = pd.read_csv('run3/customastar_maze_simulation_results.csv')
-
+astar_df = pd.read_csv('astardwa_maze_simulation_results.csv')
+custom_df = pd.read_csv('customastar_maze_simulation_results.csv')
+dynamic_df=pd.read_csv('dwa_maze_simulation_results.csv')
 
 fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 12))
 
 
 ax1.plot(astar_df['Difficulty'], astar_df['Avg Path Length'], marker='o', label='A* DWA')
 ax1.plot(custom_df['Difficulty'], custom_df['Avg Path Length'], marker='s', label='A*(Global)')
-ax1.plot(dynamic_df['Difficulty'], dynamic_df['Avg Path Length'], marker='g', label='Dynamic Window(local)')
+ax1.plot(dynamic_df['Difficulty'], dynamic_df['Avg Path Length'], marker='s', label='Dynamic Window(local)')
 ax1.set_xlabel('Difficulty')
 ax1.set_ylabel('Average Path Length')
 ax1.set_title('Difficulty vs Average Path Length')
@@ -20,7 +20,7 @@ ax1.grid(True)
 
 ax2.plot(astar_df['Difficulty'], astar_df['Avg Time'], marker='o', label='A* DWA(My implementation)')
 ax2.plot(custom_df['Difficulty'], custom_df['Avg Time'], marker='s', label='A*(Global)')
-ax2.plot(dynamic_df['Difficulty'], dynamic_df['Avg Time'], marker='g', label='Dynamic Window(local)')
+ax2.plot(dynamic_df['Difficulty'], dynamic_df['Avg Time'], marker='s', label='Dynamic Window(local)')
 ax2.set_xlabel('Difficulty')
 ax2.set_ylabel('Average Time')
 ax2.set_title('Difficulty vs Average Time')
